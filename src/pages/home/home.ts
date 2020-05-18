693bda84a599a47be2c3f226ed25986b7d2ce6b4
Devesh mishra <deveshmishra34@gmail.com>
2020-05-18 17:37:24 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
+import {Facebook, FacebookLoginResponse} from "@ionic-native/facebook";
+
+declare const cordova;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private fb: Facebook,
+    private platform: Platform
+  ) {
+
+    // if (cordova.plugins)  {
+    // } else {
+    //   console.log('codeplayfacebookads not exist');
+    // }
+
+    this.platform.ready().then( () => {
+      console.log('codeplayfacebookads exist', JSON.stringify(cordova.plugins.codeplayfacebookads));
+    })
 
   }
 
+  async facebookLogin() {
+
+    console.log('Login Facebook');
+    try {
+      const resp = await this.fb.login(['email', 'public_profile']);
+      console.log('Logged into Facebook!', JSON.stringify(resp));
+      const profile =  await this.fb.api(`me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)`, [])
+      const userData = {email: profile[`email`], first_name: profile[`first_name`], picture: profile[`picture_large`][`data`][`url`], username: profile[`name`] }
+
+      console.log('Facebook Api Resp', JSON.stringify(userData));
+    } catch (e) {
+      console.log('Error logging into Facebook', e);
+    }
+  }
+
+  async facebookLogout() {
+    await this.fb.logout()
+  }
 }
